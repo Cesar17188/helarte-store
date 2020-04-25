073f91b232904b734102f8111efa86d8d56715bd
Cesar Armendariz <cesareli17188@gmail.com>
2020-04-24 20:23:33 -0500

    layout completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,65 +11,72 @@ import { IcecreamDetailComponent } from './icecream-detail/icecream-detail.compo
 import { ProductsComponent } from './products/products.component';
 import { CafesproductsComponent } from './cafesproducts/cafesproducts.component';
 import { SnacksproductsComponent } from './snacksproducts/snacksproducts.component';
+import { LayoutComponent } from './layout/layout.component';
+
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'productos',
-    component: ProductsComponent
-  },
-  {
-    path: 'helados',
-    component: IceproductsComponent
-  },
-  {
-    path: 'helados/:codigo',
-    component: IcecreamDetailComponent
-  },
-  {
-    path: 'sabores',
-    component: SaboresComponent
-  },
-  {
-    path: 'backeries',
-    component: BackeriesproductsComponent
-  },
-  {
-    path: 'backeries/:codigo',
-    component: BackeriesproductsComponent
-  },
-  {
-    path: 'cafes',
-    component: CafesproductsComponent
-  },
-  {
-    path: 'cafes/:codigo',
-    component: CafesproductsComponent
-  },
-  {
-    path: 'snacks',
-    component: SnacksproductsComponent
-  },
-  {
-    path: 'snacks/:codigo',
-    component: SnacksproductsComponent
-  },
-  {
-    path: 'contacto',
-    component: ContactComponent
-  },
-  
-  {
-    path: '**',
-    component: PageNotFoundComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'productos',
+        component: ProductsComponent
+      },
+      {
+        path: 'helados',
+        component: IceproductsComponent
+      },
+      {
+        path: 'helados/:codigo',
+        component: IcecreamDetailComponent
+      },
+      {
+        path: 'sabores',
+        component: SaboresComponent
+      },
+      {
+        path: 'backeries',
+        component: BackeriesproductsComponent
+      },
+      {
+        path: 'backeries/:codigo',
+        component: BackeriesproductsComponent
+      },
+      {
+        path: 'cafes',
+        component: CafesproductsComponent
+      },
+      {
+        path: 'cafes/:codigo',
+        component: CafesproductsComponent
+      },
+      {
+        path: 'snacks',
+        component: SnacksproductsComponent
+      },
+      {
+        path: 'snacks/:codigo',
+        component: SnacksproductsComponent
+      },
+      {
+        path: 'contacto',
+        component: ContactComponent
+      },
+      {
+        path: '**',
+        component: PageNotFoundComponent
+      },
+    ]
   },
 ];
 
