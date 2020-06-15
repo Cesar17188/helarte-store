47904c42ed8ecce11d255f2685e64f4797e59b9e
Cesar Armendariz <cesareli17188@gmail.com>
2020-06-15 17:31:38 -0500

    arreglos en el service de helado

--- src/app/core/services/helados/helados.service.ts
+++ src/app/core/services/helados/helados.service.ts
@@ -7,73 +7,6 @@ import { Product } from '../../../product.model';
 })
 export class HeladosService {
 
-  helados: Product[] = [
-    {
-        codigo: 'h1',
-        image: 'assets/images/hchocolate.jpg',
-        producto: 'Cono simple',
-        sabores: [],
-        descripcion_corta: 'Cono waffer con un sabor de helado',
-        descripcion_larga: 'Cono wafer con un sabor de helado a tu elección',
-        unidadMedida: 'unidad',
-        precioVenta: 1.30
-    },
-    {
-      codigo: 'h2',
-      image: 'assets/images/hvainilla.jpg',
-      producto: 'Cono doble',
-      sabores: [],
-      descripcion_corta: 'Cono waffer con dos sabores de helado',
-      unidadMedida: 'unidad',
-      precioVenta: 2.25
-  },
-  {
-    codigo: 'h3',
-    image: 'assets/images/hchocolate.jpg',
-    producto: 'Tulipan Simple',
-    sabores: [],
-    descripcion_corta: 'Tulipan waffer con un sabor de helado',
-    unidadMedida: 'unidad',
-    precioVenta: 1.50
-  },
-  {
-    codigo: 'h4',
-    image: 'assets/images/hchocolate.jpg',
-    producto: 'Tulipan Doble',
-    sabores: [],
-    descripcion_corta: 'Tulipan waffer con dos sabores de helado',
-    unidadMedida: 'unidad',
-    precioVenta: 1.50
-  },
-  {
-    codigo: 'h5',
-    image: 'assets/images/hchocolate.jpg',
-    producto: 'Copa',
-    sabores: [],
-    descripcion_corta: 'Copa con varios sabores de helado',
-    unidadMedida: 'unidad',
-    precioVenta: 2.95
-  },
-  {
-    codigo: 'h6',
-    image: 'assets/images/hchocolate.jpg',
-    producto: 'Medio Litro',
-    sabores: [],
-    descripcion_corta: 'Medio litro de helado de un sabor',
-    unidadMedida: 'unidad',
-    precioVenta: 2.50
-  },
-  {
-    codigo: 'h7',
-    image: 'assets/images/hchocolate.jpg',
-    producto: 'Litro',
-    sabores: [],
-    descripcion_corta: 'Litro de helado de un sabor',
-    unidadMedida: 'unidad',
-    precioVenta: 2.50
-  },
-];
-
   idHelado = 'R4ryZffiU9qGbN9I4kip';
 
   constructor(
@@ -109,12 +42,4 @@ export class HeladosService {
     });
   }
 
-
-  getAllIceCream() {
-    return this.helados;
-  }
-
-  getIceCream(codigo: string){
-    return this.helados.find(item => codigo === item.codigo);
-  }
 }
