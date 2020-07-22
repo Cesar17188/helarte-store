58f025f5dfb94afc51eaf29225190db512d95d73
Cesar Armendariz <cesareli17188@gmail.com>
2020-07-22 17:23:15 -0500

    try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,6 @@ import { PageNotFoundModule } from './page-not-found/page-not-found.module';
 import { MaterialModule } from './material/material.module';
 import { ShakeModule } from './shake/shake.module';
 import { BebidaModule } from './bebida/bebida.module';
-import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 
 @NgModule({
@@ -58,34 +57,6 @@ import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
     PageNotFoundModule,
     MaterialModule,
     HttpClientModule,
-    NgxAuthFirebaseUIModule.forRoot(
-      {
-        apiKey: 'AIzaSyAFH2RruwxCO0jx-Fkz18kaBb0AvfQwdS0',
-        authDomain: 'helarte-store.firebaseapp.com',
-        databaseURL: 'https://helarte-store.firebaseio.com',
-        projectId: 'helarte-store',
-        storageBucket: 'helarte-store.appspot.com',
-        messagingSenderId: '924214414906',
-        appId: '1:924214414906:web:1faa33d1bdd2e838c0ef03',
-        measurementId: 'G-MN2D01RXJB'
-      },
-      () => 'HelarteStore',
-      {
-        enableFirestoreSync: true, // enable/disable autosync users with firestore
-        toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
-        toastMessageOnAuthError: false, // whether to open/show a snackbar message on auth error - default : true
-        authGuardFallbackURL: '/loggedout', // url for unauthenticated users - to use in combination with canActivate feature on a route
-        authGuardLoggedInURL: '/loggedin', // url for authenticated users - to use in combination with canActivate feature on a route
-        passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
-        passwordMinLength: 8, // Password length min/max in forms independently of each componenet min/max.
-        // Same as password but for the name
-        nameMaxLength: 50,
-        nameMinLength: 2,
-        // If set, sign-in/up form is not available until email has been verified.
-        // Plus protected routes are still protected even though user is connected.
-        guardProtectedRoutesUntilEmailIsVerified: true,
-        enableEmailVerification: true, // default: true
-      }),
   ],
   providers: [],
   bootstrap: [AppComponent]
